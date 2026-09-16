474071ef4403d5a94e3f057d7b877a1fc5a74fed
Fredy Idarraga <fredy@rutina.local>
2026-09-16 13:04:11 +0000

    Corregir import faltante de HistoryScreen

--- app/src/main/java/com/fredy/rutina/MainActivity.kt
+++ app/src/main/java/com/fredy/rutina/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.fredy.rutina.ui.DayDetailScreen
+import com.fredy.rutina.ui.HistoryScreen
 import com.fredy.rutina.ui.LibraryScreen
 import com.fredy.rutina.ui.WeekScreen
 import com.fredy.rutina.ui.theme.RutinaFredyTheme
